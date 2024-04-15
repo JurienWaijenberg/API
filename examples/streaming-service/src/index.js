ceac57bca0eef424a6ac789c7ffdb0a4efade567
Juriën Waijenberg <jurien.waijenberg@hva.nl>
2024-04-15 14:39:55 +0200

    Added share from current url

--- examples/streaming-service/src/index.js
+++ examples/streaming-service/src/index.js
@@ -13,23 +13,3 @@ console.log('Hello, world!');
 // }
 
 
-// Share functie
-
-const shareData = {
-    title: "MDN",
-    text: "Learn web development on MDN!",
-    url: "https://developer.mozilla.org",
-  };
-  
-  const btn = document.querySelector(".button");
-  const resultPara = document.querySelector(".result");
-  
-  // Share must be triggered by "user activation"
-  btn.addEventListener("click", async () => {
-    try {
-      await navigator.share(shareData);
-      resultPara.textContent = "MDN shared successfully";
-    } catch (err) {
-      resultPara.textContent = `Error: ${err}`;
-    }
-  });
